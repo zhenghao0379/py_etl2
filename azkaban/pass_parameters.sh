bc83df9ce2c03ed6e5924552a4559c11dee836a9
zhenghao <dpzhenghao@dfl.com.cn>
2020-05-24 15:28:43 +0800

    azkaban test

--- azkaban/pass_parameters.sh
+++ azkaban/pass_parameters.sh
@@ -7,11 +7,53 @@
 # ('-M', '--MAIL', help='邮件接收者')
 
 
+# 原始参数
+echo "原始参数：$1 $2 $3 $4 $5"
+
+# 接收参数
+if [ $1 != '${D}' ]
+then
+  DAYS = "-D $1"
+else
+  DAYS = ''
+fi
+
+if [ $2 != '${S}' ]
+then
+  START = "-S $2"
+else
+  START = ''
+fi
+
+if [ $3 != '${E}' ]
+then
+  END = "-E $3"
+else
+  END = ''
+fi
+
+if [ $4 != '${T}' ]
+then
+  RPT_TYPES = "-T $4"
+else
+  RPT_TYPES = ''
+fi
+
+if [ $5 != '${M}' ]
+then
+  MAIL = "-M $5"
+else
+  MAIL = ''
+fi
+
 echo "------输入参数------"
 echo "DAYS:$DAYS"
 echo "START:$START"
 echo "END:$END"
-echo "RPT_TYPE:$RPT_TYPE"
+echo "RPT_TYPES:$RPT_TYPES"
 echo "MAIL:$MAIL"
 echo "-------------------"
 
+py_flow_val="$DAYS $START $END $RPT_TYPES $MAIL"
+
+echo "{'py_flow_val': $py_flow_val}" > $JOB_OUTPUT_PROP_FILE
--- demo/code/test2.py
+++ demo/code/test2.py
@@ -8,13 +8,14 @@ table_name = 'ods_house'
 
 print('hello world, hello python, hello azkaban')
 
-for day in DAYS:
-    for rpt_type in RPT_TYPES:
-        print(day)
-        print(rpt_type)
+if __name__ == '__main__':
+    for day in DAYS:
+        for rpt_type in RPT_TYPES:
+            print(day)
+            print(rpt_type)
 
-        sql_day = f"select * from {table_name} where day = {day}"
-        print(sql_day)
-        host = CONFIG.get_value('mysql', 'host')
-        print(host)
-        print(type(host))
+            sql_day = f"select * from {table_name} where day = {day}"
+            print(sql_day)
+            host = CONFIG.get_value('mysql', 'host')
+            print(host)
+            print(type(host))
